ca61edb345b7c6dc6882496c2692e462847e83d0
Pericles001 <periclesadjovi@gmail.com>
2022-09-10 14:38:18 +0100

    Edit file to set database connection

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,18 +1,18 @@
-import time
-
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-while True:
-    try:
-        connector = psycopg2.connect(host=settings.POSTGRES_SERVER, database=settings.POSTGRES_DB,
-                                     user=settings.POSTGRES_USER, password=settings.POSTGRES_PASSWORD,
-                                     cursor_factory=RealDictCursor)
-        cursor = connector.cursor()
-        print("Database connection was successfully")
-        break
-    except Exception as error:
-        print("Connection to database failed")
-        print("Error: ", error)
-        time.sleep(2)
+postgres_user = settings.POSTGRES_USER
+postgres_password = settings.POSTGRES_PASSWORD
+postgres_server = settings.POSTGRES_SERVER
+postgres_port = settings.POSTGRES_PORT
+postgres_db = settings.POSTGRES_DB
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_user}:{postgres_password}@{postgres_server}:{postgres_port}/{postgres_db}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
