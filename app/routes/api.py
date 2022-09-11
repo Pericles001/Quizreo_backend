d337841148909d5ee7f1bbc164fbabc10eac6a48
Pericles001 <periclesadjovi@gmail.com>
2022-09-11 12:01:08 +0100

    Remove files

---
+++ app/routes/api.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from app.endpoints import answer, party, quiz, survey, trial, user
+
+# from app.endpoints import answer, party, quiz, survey, trial, user
+router = APIRouter()
+
+router.include_router(answer.router)
+router.include_router(party.router)
+router.include_router(quiz.router)
+router.include_router(survey.router)
+router.include_router(trial.router)
+router.include_router(user.router)
